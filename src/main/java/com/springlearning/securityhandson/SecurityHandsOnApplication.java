ec75fbf0cf0fd7df1cc97732eacabb5dc9f8738b
Suriya <suriyasmart555@gmail.com>
2021-04-18 10:55:27 +0530

    Initial commit

---
+++ src/main/java/com/springlearning/securityhandson/SecurityHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.springlearning.securityhandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityHandsOnApplication.class, args);
+	}
+
+}
